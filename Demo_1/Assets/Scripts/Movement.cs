627bd7d3aca02d1c5720b9163f2329e1e855e6bc
madison agostino <madison.agostino@createthenext.com>
2019-08-05 16:29:56 -0400

    vr

---
+++ Demo_1/Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    
+            //List of variables
+            public GameObject player2;
+            public float Speed = 1.5f;
+            // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+            // Update is called once per frame
+    void Update()
+    {
+       transform.position += Vector3.right * Speed * Time.deltaTime;
+
+       player2.transform.position += direction * Speed * Time.deltaTime;
+
+    }
+}
---
+++ VR_Project/Assets/Script/Moving.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+        //Variables
+            
+            public float speed = 0.5f;    // used to control the speed of our movement
+            public GameObject Camera;   //This will be used to reference our camera 
+        
+        // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+        // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space)) {
+
+        transform.position = transform.position + Camera.transform.forward * speed * Time.deltaTime;
+        
+        }
+
+        else if (Input.touchCount > 0)
+        {
+            transform.position = transform.position + Camera.transform.forward * speed * Time.deltaTime
+        }
+
+    }
+}
